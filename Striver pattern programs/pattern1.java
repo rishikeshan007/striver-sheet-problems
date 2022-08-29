c0c7536d663f93046bdeac30723a3203be57accd
rishikeshan007 <65391753+rishikeshan007@users.noreply.github.com>
2022-08-29 09:37:48 +0530

    Create pattern1.java

---
+++ Striver pattern programs/pattern1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+	    int n = input.nextInt();
+	    for(int i=1;i<=n;i++)
+	    {
+	        for(int j=1;j<=n;j++)
+	        {
+	            System.out.print("*");
+	        }
+	        System.out.println();
+	    }
+		
+	}
+}
+
+o/p:
+*****
+*****
+*****
+*****
+*****
