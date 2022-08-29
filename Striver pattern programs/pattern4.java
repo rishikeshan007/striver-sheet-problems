641317238c6ff40e2f12b6d8da7bfd8eec0e5eae
rishikeshan007 <65391753+rishikeshan007@users.noreply.github.com>
2022-08-29 09:42:52 +0530

    Create pattern4.java

---
+++ Striver pattern programs/pattern4.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+	    int n = input.nextInt();
+	    for(int i=1;i<=n;i++)
+	    {
+	        for(int j=1;j<=i;j++)
+	        {
+	            System.out.print(i+" ");
+	        }
+	        System.out.println();
+	    }
+	   
+		
+	}
+}
+
+o/p:
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5  
